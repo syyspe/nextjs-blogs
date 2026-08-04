bef3b6f024452c3dd66aad85005c7bfbf8ad8af1
syyspe <34917365+syyspe@users.noreply.github.com>
2026-08-04 12:58:10 +0300

    tehtava 4

--- app/services/blogs.ts
+++ app/services/blogs.ts
@@ -13,10 +13,16 @@ export const getBlogs = () => {
 
 export const getBlogById = (id: number) => {
   const blog = blogs.find(value => value.id === id) || undefined
-  console.log(id, blog)
   return blog
 }
 
 export const addBlog = (title: string, author: string, url: string, likes: number) => {
   blogs.push({ id: nextId++, title, author, url, likes })
+}
+
+export const like = (id: number) => {
+  const blog = blogs.find(value => value.id === id) || undefined
+  // console.log("like", id, blog)
+  if (blog) blog.likes++
+  // console.log("like", id, blog)
 }
